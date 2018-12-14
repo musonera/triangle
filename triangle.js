02c866f56a564a9138ce585a7fa45862557df375
musonera <musosoaimee@gmail.com>
2018-12-14 15:16:09 +0200

    changing some codes

--- triangle.js
+++ triangle.js
@@ -1,17 +1,18 @@
 function myFunction(){
-var side1 = parseInt(document.getElementById(side1).value);
-var side2 = parseInt(document.getElementById(side2).value);
-var side3 = parseInt(document.getElementById(side3).value);
-if (side1==side2 && side1==side3 && side2==side3){
+var side1 = document.getElementById(sideOne).value;
+var side2 = document.getElementById(sideTwo).value;
+var side3 = document.getElementById(sideThree).value;
+
+if (side1===side2 && side1===side3 && side2===side3){
     alert("Equilateral");
-    console.log("Equilateral");
 }
-    else if(side1==side2 && side1!==side3 && side2!==side3) {
+    else if(side1===side2 || side1===side3 || side2===side3) {
         alert("Isosceles");
-        console.log("Isosceles");
     }
     else if(side1!==side2 && side1!==side3 && side2!==side3){
         alert("Scalene");
-        console.log("Scalene");
+    }
+    else{
+        alert("not a triangle");
     }
 }
