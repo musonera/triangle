09963c8926fcf39292ed7cb8bfde9153603edbee
musonera <musosoaimee@gmail.com>
2018-12-15 08:21:42 +0200

    changing function name

--- triangle.js
+++ triangle.js
@@ -1,4 +1,4 @@
-function myFunction(){
+function myProject(){
 var side1 = document.getElementById(side1).value;
 var side2 = document.getElementById(side2).value;
 var side3 = document.getElementById(side3).value;
