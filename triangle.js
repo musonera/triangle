dffc801286602aaec8e124f36ccc425778fa30bb
musonera <musosoaimee@gmail.com>
2018-12-15 08:31:38 +0200

    adding quote tomy js

--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 function myProject(){
-var side1 = document.getElementById(side1).value;
-var side2 = document.getElementById(side2).value;
-var side3 = document.getElementById(side3).value;
+var side1 = document.getElementById("side1").value;
+var side2 = document.getElementById("side2").value;
+var side3 = document.getElementById("side3").value;
 
 if (side1===side2 && side1===side3 && side2===side3){
    console.log( alert("Equilateral"));
