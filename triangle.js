c2109c7d7b6034c813cb8977fe3b932275237256
musonera <musosoaimee@gmail.com>
2018-12-15 08:10:24 +0200

    changing some inputs

--- triangle.js
+++ triangle.js
@@ -1,14 +1,15 @@
 function myFunction(){
-var side1 = document.getElementById(sideOne).value;
-var side2 = document.getElementById(sideTwo).value;
-var side3 = document.getElementById(sideThree).value;
+var side1 = document.getElementById(side1).value;
+var side2 = document.getElementById(side2).value;
+var side3 = document.getElementById(side3).value;
 
 if (side1===side2 && side1===side3 && side2===side3){
-    alert("Equilateral");
+   console.log( alert("Equilateral"));
 }
     else if(side1===side2 || side1===side3 || side2===side3) {
         alert("Isosceles");
     }
+
     else if(side1!==side2 && side1!==side3 && side2!==side3){
         alert("Scalene");
     }
