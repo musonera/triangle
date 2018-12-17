2dab866bdc418d4340f32866f94af63f7084a3a6
musonera <musosoaimee@gmail.com>
2018-12-17 17:28:30 +0200

    add style in css

--- triangle.js
+++ triangle.js
@@ -2,13 +2,14 @@ function myProject(){
 var side1 = parseInt(document.getElementById("side1").value);
 var side2 = parseInt(document.getElementById("side2").value);
 var side3 = parseInt(document.getElementById("side3").value);
-var sides =[ side1,side2,side3];
+var sides =["side1","side2","side3"];
 sides.forEach(function(side){
-    console.log("number of sides" +  side1  );
+    console.log(" side's number is " +  side  );
 });
 side1[0];
 side2[1];
 side3[2];
+
 if(side1+side2>side3 &&
     side1+side3>side2 &&
     side2+side3>side1){
