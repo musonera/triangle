1c7ba3be9e7a5fc96edb9d1249b87296e78b0b62
musonera <musosoaimee@gmail.com>
2018-12-17 11:09:34 +0200

    add new condition to my function

--- triangle.js
+++ triangle.js
@@ -13,7 +13,10 @@ if (side1===side2 && side1===side3 && side2===side3){
     else if(side1!==side2 && side1!==side3 && side2!==side3){
         alert("Scalene");
     }
-    else{
+    else if(side1+side2<=side3 ||side1+side3<=side2 ||side2+side3<=side1){
         alert("not a triangle");
+    }
+    else{
+        alert("invalid");
     }
 }
