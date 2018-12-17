388bba061ca0a062a5962f51ee9a94ff84c002a6
musonera <musosoaimee@gmail.com>
2018-12-17 12:31:33 +0200

    changing my background

--- triangle.js
+++ triangle.js
@@ -13,7 +13,7 @@ if (side1===side2 && side1===side3 && side2===side3){
     else if(side1!==side2 && side1!==side3 && side2!==side3){
         alert("Scalene");
     }
-    else if(side1+side2<=side3 ||side1+side3<=side2) {
+    else if(side1+side2<=side3 )||(side1+side3<=side2) ||(side2+side3<=side1){
         alert("not a triangle");
     }
     else{
